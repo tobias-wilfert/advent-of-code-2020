baf4020d0a0de98e21daa6c5a5ffb756ea18d20e
tobias-wilfert <s0181439@ad.ua.ac.be>
2020-12-02 08:18:49 +0100

    Added solution for day two + input file

--- main.cpp
+++ main.cpp
@@ -3,39 +3,34 @@
 #include <fstream>
 
 #include <vector>
-#include <unordered_map>
+#include <unordered_set>
 
 /**
- * Answer for https://adventofcode.com/2020/day/1 part one
- * Methodology:
- *  Make a map that has as a key 2020-x and as a value the index of x.
- *  Then loop over the list and check if the map has a complement for the
- *  current value, if yes return the product of the two.
+ * Answer for https://adventofcode.com/2020/day/1
  * Analysis:
  * - Space Complexity: O(n)
  * - Time Complexity: O(n)
  * @note If no two entries satisfy the requirement the function returns -1
  * @param vec Expense report from the Elves in accounting
  * @return the product of the two entries in vec that sum up to 2020
- * @author: Tobias Wilfert
  */
 int solutionDayOnePartOne(const std::vector<int> &vec) {
-  std::unordered_map<int, int> complement_map;
+  std::unordered_set<int> complement_set;
 
-  for (int i = 0; i < vec.size(); ++i) {
-    complement_map[2020 - vec[i]] = i;
+  for (int num : vec) {
+    complement_set.insert(2020 - num);
   }
 
-  for (int i = 0; i < vec.size(); ++i) {
-    if (complement_map.find(vec[i])!=complement_map.end()) {
-      return vec[i]*vec[complement_map[vec[i]]];
+  for (int num : vec) {
+    if (complement_set.find(num)!=complement_set.end()) {
+      return num*(2020 - num);
     }
   }
   return -1;
 }
 
 /**
- * Answer for https://adventofcode.com/2020/day/1 part two
+ * Answer for https://adventofcode.com/2020/day/1#part2
  * Analysis:
  * - Space Complexity: O(1)
  * - Time Complexity: O(n^3)
@@ -43,7 +38,6 @@ int solutionDayOnePartOne(const std::vector<int> &vec) {
  * @note If no three entries satisfy the requirement the function returns -1
  * @param vec Expense report from the Elves in accounting
  * @return the product of the three entries in vec that sum up to 2020
- * @author: Tobias Wilfert
  */
 int solutionDayOnePartTwo(const std::vector<int> &vec) {
   for (int i = 0; i < vec.size(); ++i) {
@@ -58,6 +52,64 @@ int solutionDayOnePartTwo(const std::vector<int> &vec) {
   return -1;
 }
 
+/**
+ * Answer for https://adventofcode.com/2020/day/2
+ * Analysis:
+ * - Space Complexity: O(1)
+ * - Time Complexity: O(n)
+ * @param input Vector of tuples, where each tuple is of the form:
+ * <lower_bound, upperbound, character, password>
+ * @return The number of passwords that are valid
+ */
+int solutionDayTwoPartOne(const std::vector<std::tuple<int,
+                                                       int,
+                                                       char,
+                                                       std::string>> &input) {
+  int count = 0;
+
+  for (const auto &t: input) {
+    int char_count = 0;
+    for (const char &c: std::get<3>(t)) {
+      if (c==std::get<2>(t)) {
+        ++char_count;
+      }
+    }
+    if (std::get<0>(t) <= char_count && char_count <= std::get<1>(t)) {
+      ++count;
+    }
+  }
+
+  return count;
+}
+
+/**
+ * Answer for https://adventofcode.com/2020/day/2#part2
+ * Analysis:
+ * - Space Complexity: O(1)
+ * - Time Complexity: O(n)
+ * @param input Vector of tuples, where each tuple is of the form:
+ * <lower_bound, upperbound, character, password>
+ * @return The number of passwords that are valid
+ */
+int solutionDayTwoPartTwo(const std::vector<std::tuple<int,
+                                                       int,
+                                                       char,
+                                                       std::string>> &input) {
+  int count = 0;
+
+  for (const auto &t: input) {
+    char first_position = std::get<3>(t).at(std::get<0>(t) - 1);
+    char second_position = std::get<3>(t).at(std::get<1>(t) - 1);
+    char correct = std::get<2>(t);
+    if (first_position!=second_position
+        && (first_position==correct || second_position==correct)) {
+      ++count;
+    }
+  }
+
+  return count;
+}
+
 int main() {
   // Read the input into a vector
   std::ifstream is("../inputs/dayOne.txt");
@@ -68,5 +120,25 @@ int main() {
   std::cout << "Solution day one, part one: " << solutionDayOnePartOne(input)
             << std::endl;
   std::cout << "Solution day one, part two: " << solutionDayOnePartTwo(input)
+            << std::endl;
+
+
+  // Read the input for day two
+  std::ifstream infile("../inputs/dayTwo.txt");
+  std::string count, character, password;
+  std::vector<std::tuple<int, int, char, std::string>> inputDayTwo;
+
+  while (infile >> count >> character >> password) {
+    int lowerBound = std::stoi(count.substr(0, count.find_first_of('-')));
+    int upperBound = std::stoi(count.substr(count.find_first_of('-') + 1));
+    inputDayTwo.emplace_back(lowerBound, upperBound, character[0], password);
+  }
+
+  // Day two
+  std::cout << "Solution day two, part one: "
+            << solutionDayTwoPartOne(inputDayTwo)
+            << std::endl;
+  std::cout << "Solution day two, part two: "
+            << solutionDayTwoPartTwo(inputDayTwo)
             << std::endl;
 }
